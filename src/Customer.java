820c5e663d65cbad88c522e6f30ad9117a96cd8e
Moukaskon <moukaskon04@gmail.com>
2025-05-19 17:41:32 +0300

    Initial

---
+++ src/Customer.java
@@ -0,0 +1,21 @@
+public class Customer {
+    private String name;
+    private Account account;
+
+    public Customer(String name, Account account) {
+        this.name = name;
+        this.account = account;
+    }
+
+    public double getAccountBalance() {
+        return account.getDeposits() - account.getWithdrawals();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
